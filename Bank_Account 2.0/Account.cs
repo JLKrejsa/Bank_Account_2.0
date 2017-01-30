dc499c0b4bed00dbce5a480260f31a991e6b9ac3
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-01-30 13:49:20 -0500

    first commit

---
+++ Bank_Account 2.0/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_2._0
+{
+    class Account
+    {
+        //fields
+        private string clientName = "Gillain Kate";
+        private int accountNumber = 020906;
+        private float beginBalance = 0;
+
+        //properties
+        public string ClientName
+        {
+            get { return this.clientName; }
+            set { this.clientName = "Gillian Kate"; }
+        }
+
+        protected int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = 020906; }
+        }
+
+        protected float BeginBalance
+        {
+            get { return this.beginBalance; }
+            set { this.beginBalance = 0; }
+        }
+
+        //methods
+        public void AccountSummary()
+        {
+            Console.WriteLine("Name " + clientName);
+            Console.WriteLine("Account number " + accountNumber);
+        }
+
+        public void Balance()
+        {
+            Console.WriteLine("Balance " + this.beginBalance);
+        }
+
+
+        public void Deposit()
+        {
+
+        }
+    }
+}
---
+++ Bank_Account 2.0/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //menu
+            Console.WriteLine("Welcome to the Bank of Krejsa-Kupresanin");
+            Console.WriteLine("How may we serve you today?");
+
+            Account account1 = new Account();
+            Account balance1 = new Account();
+
+            List<string> Menu = new List<string>();
+            while (true)
+            {
+                Menu.Add("\n");
+                Menu.Add("Enter 1 for Account Summary");
+                Menu.Add("Enter 2 for Account Balance");
+                Menu.Add("Enter 3 to Deposit");
+                Menu.Add("Enter 4 to Withdraw");
+                Menu.Add("Enter 5 to Exit");
+                Menu.ForEach(Console.WriteLine);
+                int menuChoice = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                if (menuChoice == 1)
+                {
+                    account1.AccountSummary();
+                }
+
+                else if (menuChoice == 2) ;
+                {
+                    balance1.Balance();
+                }
+                //else if (menuChoice == 3) ;
+                //{
+
+                //}
+
+                //end of while loop
+            }
+            //else if (menuChoice == 3) ;
+            //{
+
+            //}
+
+
+            //end Main
+        }
+    }
+}
